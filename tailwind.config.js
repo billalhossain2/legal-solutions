01a4a5af84188f3f5ce4ca130962e65291460011
billal-webdev <billal-webdev@gmail.com>
2023-07-24 19:28:27 +0600

    basic setup is completed

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily:{
+        garamond:['EB Garamond', 'serif'],
+        poppins:['Poppins', 'sans-serif'],
+        sans: ['Poppins', 'sans-serif'],
+        serif: ['Merriweather', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
